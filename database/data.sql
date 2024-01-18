e48c97136e6aa7302f3fdde191d8d8b4b4c8f669
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2024-01-18 00:38:54 +0000

    Initial commit

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,9 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+insert into "parks"
+  ("parkId", "parkName", "longitude", "lattitude")
+  values
+    ('832fcd51-ea19-4e77-85c7-75d5843b127c', 'Disney California Adventure Park', '-117', '33' ),
+    ('75ea578a-adc8-4116-a54d-dccb60765ef9', 'Magic Kingdom Park', '-81', '28')
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,35 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "username" text,
+  "hashedPassword" text,
+  "fullName" text,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "parks" (
+  "parkId" text PRIMARY KEY,
+  "parkName" text,
+  "longitude" integer,
+  "lattitude" integer
+);
+
+CREATE TABLE "userAttractions" (
+  "userId" integer,
+  "attractionId" text,
+  "parkId" text,
+  PRIMARY KEY ("userId", "attractionId", "parkId")
+);
+
+COMMENT ON COLUMN "parks"."parkId" IS 'from api';
+
+COMMENT ON COLUMN "userAttractions"."attractionId" IS 'from api';
+
+COMMENT ON COLUMN "userAttractions"."parkId" IS 'from api';
+
+ALTER TABLE "userAttractions" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "userAttractions" ADD FOREIGN KEY ("parkId") REFERENCES "parks" ("parkId");
